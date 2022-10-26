6c5ea2a75fb6f862ccaa7d04ec0c7f198678a156
Fahima01 <fahimaakter2014@gmail.com>
2022-10-26 11:59:22 +0600

    components layout commit

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -3,12 +3,15 @@ import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import RightSideNav from '../pages/Shared/Right_side_nav/RightSideNav';
+import Header from '../pages/Shared/Header/Header';
+import Footer from '../pages/Shared/Footer/Footer';
 const Main = () => {
     return (
 
         <div>
+            <Header></Header>
             <Container>
-
                 <Row>
                     <Col xs={12} md={8} sm={12} lg='9' className='bg-primary' >
                         <Outlet></Outlet>
@@ -16,9 +19,11 @@ const Main = () => {
                     </Col>
                     <Col xs={12} md={4} sm={12} lg='3' className='bg-info' >
                         <h2>This is sidebar container</h2>
+                        <RightSideNav></RightSideNav>
                     </Col>
                 </Row>
             </Container>
+            <Footer></Footer>
         </div>
 
     );
---
+++ src/pages/Language_courses/LanguageCourses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LanguageCourses = () => {
+    return (
+        <div>
+            <h2>Language Courses</h2>
+        </div>
+    );
+};
+
+export default LanguageCourses;
