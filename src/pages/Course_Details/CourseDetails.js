e2866c41e90710f9fc27be0fafeed883438fb7ef
Fahima01 <fahimaakter2014@gmail.com>
2022-10-28 14:37:16 +0600

    card design commit

--- src/pages/Course_Details/CourseDetails.js
+++ src/pages/Course_Details/CourseDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardGroup, Col, Container, Row } from 'react-bootstrap';
+import { Card, CardGroup, Col, Container, Image, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -8,18 +8,38 @@ import RightSideNav from '../Shared/Right_side_nav/RightSideNav';
 
 const CourseDetails = () => {
     const courseDetails = useLoaderData()
-    const { id, title, rating, total_student, image_url, details } = courseDetails;
+    const { id, title, rating, total_student, image_url, details, mentor } = courseDetails;
     return (
         <div>
 
             <Container>
                 <Row>
                     <Col xs={12} md={12} sm={12} lg='8' >
-                        <CardGroup>
+                        <CardGroup className='mt-5'>
 
                             <Card className='card shadow-lg' >
-                                <Card.Header>
+                                <Card.Header className='d-flex justify-content-between align-items-center'>
 
+                                    <div className='d-flex'>
+                                        <div>
+                                            <Image
+                                                roundedCircle
+                                                className='me-2 '
+                                                src={mentor.img}
+                                                style={{ height: '50px', width: '50px' }}
+                                            ></Image>
+                                        </div>
+                                        <div>
+                                            <p>{mentor.name}</p>
+                                            <p>{mentor.published_date}</p>
+                                        </div>
+                                    </div>
+
+                                    <div className='d-flex'>
+                                        <p className='me-3 '>
+                                            <span className='me-2 text-warning'><FaStar></FaStar> </span> {rating.number} </p>
+                                        <p className='fw-bold'>Total View: <span className='text-info'> {total_student} </span></p>
+                                    </div>
 
                                 </Card.Header>
                                 <Card.Img className='detail-card-img' variant="top" src={image_url} />
@@ -33,9 +53,7 @@ const CourseDetails = () => {
                                 </Card.Text>
                                 <Card.Footer className='py-3'>
                                     <div className='d-flex justify-content-evenly align-items-center'>
-                                        <p className='d-flex justify-content-evenly align-items-center'>
-                                            <span className='me-2 text-warning'><FaStar></FaStar> </span> {rating.number} </p>
-                                        <p>Total View: <span className='text-info'> {total_student} </span></p>
+
 
 
                                     </div>
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -5,14 +5,14 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import img4 from '../../../slider-img/f073a3ecf58cdd98f2a38a17b11d8f84.png';
+import img4 from '../../../slider-img/LanguageLA.png';
 
 
 const Header = () => {
     return (
         <Navbar className='shadow-lg' bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#" ><img style={{ maxHeight: '100px', width: '100px' }} src={img4} alt="" /></Navbar.Brand>
+                <Navbar.Brand href="#" ><img style={{ maxHeight: '200px', width: '200px' }} src={img4} alt="" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
